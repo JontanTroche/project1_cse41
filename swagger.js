57629972a970008107f03fe41fb23b1a619f8391
Jonatan Troche <trochealmediajonatan@gmail.com>
2026-01-17 13:17:42 -0400

    requirements update

--- swagger.js
+++ swagger.js
@@ -6,7 +6,17 @@ const doc = {
         description: 'Users Api'
     },
     host: 'localhost:3000',
-    schemes: ['http']
+    schemes: ['http'],
+
+    definitions: {
+        User: {
+            firstName: "test1",
+            lastName: "test1",
+            email: "test1@gmail.com",
+            favoriteColor: "test1",
+            birthday: "test1"
+        }
+    }
 };
 
 const outputFile = './swagger.json';
